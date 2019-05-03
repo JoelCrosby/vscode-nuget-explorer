c8d7ab5d2328f0a8698eecd28c42b922f94c1a25
Joel Crosby <JoelCrosby@live.co.uk>
2019-05-03 04:03:09 +0100

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@
 'use strict';
 
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 /**@type {import('webpack').Configuration}*/
 const config = {
-    target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+    target: 'node',
 
-    entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    entry: path.resolve(__dirname, './src/extension.ts'),
     output: {
-        // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
         path: path.resolve(__dirname, 'dist'),
         filename: 'extension.js',
         libraryTarget: 'commonjs2',
@@ -18,11 +18,10 @@ const config = {
     },
     devtool: 'source-map',
     externals: {
-        vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+        vscode: 'commonjs vscode'
     },
     resolve: {
-        // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-        extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js',]
     },
     module: {
         rules: [
@@ -36,6 +35,15 @@ const config = {
                 ]
             }
         ]
+    },
+    plugins: [
+        new CopyPlugin([
+            { from: 'resources', to: 'resources' }
+        ]),
+    ],
+    node: {
+        __dirname: false
     }
 };
+
 module.exports = config;
