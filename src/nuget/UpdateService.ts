ed2395ea566517d7c876fc8d2aad47b0babd817e
Joel Crosby <JoelCrosby@live.co.uk>
2019-05-08 22:45:53 +0100

    split update service functionality into more functions

--- src/nuget/UpdateService.ts
+++ src/nuget/UpdateService.ts
@@ -4,9 +4,20 @@ import { NugetPackage } from '../models/NugetPackage';
 class UpdateService {
 
     async checkForUpdates(nugetPackage: NugetPackage): Promise<string[]> {
+
         const versions = await this.getVersions(nugetPackage.id);
 
         if (!versions || !nugetPackage.version) { return []; }
+
+        nugetPackage.versions = versions;
+
+        return this.getUpdates(nugetPackage);
+    }
+
+    private getUpdates(nugetPackage: NugetPackage) {
+        const { versions } = nugetPackage;
+
+        if (!versions) { return []; }
 
         const currentVersionIndex = versions.findIndex(version => version === nugetPackage.version);
 
@@ -27,7 +38,7 @@ class UpdateService {
         return updates;
     }
 
-    async getVersions(packageId: string): Promise<string[]> {
+    private async getVersions(packageId: string): Promise<string[]> {
         const results = await nugetApiService.search(packageId);
 
         if (!results) { return []; }
