d5d6d242c58de9d298ea27ed14b6156f71524d9f
Joel Crosby <JoelCrosby@live.co.uk>
2019-06-08 22:58:36 +0100

    intercept httpclient erros

--- src/nuget/NuGetApiService.ts
+++ src/nuget/NuGetApiService.ts
@@ -1,18 +1,19 @@
 import Axios from 'axios';
 
-import { showErrorMessage } from '../utils';
 import { SearchResult } from './SearchResult';
 
 class NugetApiService {
 
     async search(query: string): Promise<SearchResult | undefined> {
+
         try {
             const res = await Axios.get<SearchResult>(
                 `https://api-v2v3search-0.nuget.org/query?q=${query}&prerelease=false`);
+
             return res.data;
+
         } catch (error) {
-            showErrorMessage('Unable to reach NuGet api');
-            return;
+            throw Error('Unable to reach NuGet Service.');
         }
     }
 }
